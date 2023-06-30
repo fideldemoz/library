4f526827eb6ff40af759f59336e063e715d1e30c
Fidel Livino Pedro <fidellplivino98@gmail.com>
2023-06-30 18:08:47 +0200

    Refactor code to class - restyle table to mobile devices

--- main.js
+++ main.js
@@ -1,108 +1,84 @@
-"use strict"
-
-let myLibrary = [];
-const form = document.querySelector("form");
-const modal = document.querySelector(".modal");
-const showForm = document.querySelector("#show-form");
-const alert = document.querySelector(".alert")
-
-function Book (title,author,pages,read) {
-	this.title = title
-	this.author = author
-	this.pages = pages
-	if (read === true) {
-		this.read = 'Read'
-	} else {
-		this.read = 'Not read yet'
-	}
-
-}
-
-function addBookToLibrary (book) {
-
-	function bookExists (obj){
-		return obj.title === book.title;
-	}
-
-	function alertBookExists () {
-		alert.textContent = ''
-	}
-
-	if (!myLibrary.some(bookExists)) {
-		myLibrary.push(book);
-		renderBookstoHtml(book);
-	} else {
-		alert.textContent = 'Error: book title exists in the Library'
-		window.setInterval( alertBookExists, 3000)
-	}
-}
-
-function newBook () {
-	const title = document.querySelector("#title").value;
-	const author = document.querySelector("#author").value;
-	const pages = document.querySelector("#pages").value;
-	const read = document.querySelector("#read").checked;
-	const book = new Book(title,author,pages,read);
-	addBookToLibrary(book);
-}
-
-function renderBookstoHtml(obj) {
-	const bookTable = document.querySelector(".books-table");
-	const wrapper = document.createElement("div");
-	const tNode = document.createElement("h2");
-	const aNode = document.createElement("h3");
-	const pNode = document.createElement("p");
-	const rNode = document.createElement("p");
-	const dNode = document.createElement("button");
-	const uNode = document.createElement("button");
-	const title = obj.title;
-	dNode.textContent = "Delete";
-	uNode.textContent = "Change read status";
-	dNode.classList.add("delete");
-	uNode.addEventListener("click", () => {
-		if (obj.read === "Read") {
-			obj.read = "Not read yet";
-			uNode.textContent = "Mark as read";
-		} else {
-			obj.read = "Read";
-			uNode.textContent = "Mark as not read";
+(function app () {
+	let shelf = [];
+	class Library {
+		constructor (title, author, pages, read) {
+			this.title = title;
+			this.author = author;
+			this.pages = `${pages} pages`;
+			this.read = (() => {return read === true ? "read" : "not read"})();
 		}
-	rNode.textContent = obj.read;
-	})
-	rNode.style.color = 'gray';
-	tNode.textContent = `Title: ${obj.title}`;
-	aNode.textContent = `By ${obj.author}`;
-	pNode.textContent = `No. of pages: ${obj.pages}`;
-	rNode.textContent = obj.read;
-	wrapper.appendChild(tNode);
-	wrapper.appendChild(aNode);
-	wrapper.appendChild(pNode);
-	wrapper.appendChild(rNode);
-	wrapper.appendChild(dNode);
-	wrapper.appendChild(uNode);
-	bookTable.appendChild(wrapper);
-
-	dNode.addEventListener("click", () => {
-		bookTable.removeChild(wrapper);
-		myLibrary = myLibrary.filter((obj) => obj.title !== title);
-	})
-}
-
-function formToggle() {
-	if (modal.style.display === 'block') {
-		modal.style.display = 'none'
-		showForm.textContent = 'Show Form';
-	} else {
-		modal.style.display = 'block';
-		showForm.textContent = 'Hide Form';
-	}
-}
-
-showForm.addEventListener("click", formToggle)
-
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	newBook();
-	form.reset();
-	formToggle();
-})
+		addToShelf (newBook) {
+			if (!bookExists()) {
+				shelf.push(newBook);
+				let index = shelf.length;
+				this.renderToHtml(newBook,index);
+			}
+			function bookExists () {
+				let ex = (book) => { return book.title === newBook.title};
+				return shelf.some(ex);
+		}}
+		renderToHtml (element, i) {
+			let titleNode = document.createElement("td"),
+			authorNode = document.createElement("td"),
+			pagesNode = document.createElement("td"),
+			readNode = document.createElement("td"),
+			tr = document.createElement("tr"),
+			indexNode = document.createElement("td"),
+			alertgreen = document.querySelector(".alert.green"),
+			alert = document.querySelector(".alert"),
+			tbody = document.querySelector("tbody");
+			indexNode.textContent = i;
+			titleNode.textContent = element.title;
+			authorNode.textContent = element.author;
+			pagesNode.textContent = element.pages;
+			readNode.textContent = element.read;
+			readNode.className = "read";
+			tr.appendChild(indexNode);
+			tr.appendChild(titleNode);
+			tr.appendChild(authorNode);
+			tr.appendChild(pagesNode);
+			tr.appendChild(readNode);
+			tbody.appendChild(tr);
+			remove(tr);
+			readNode.addEventListener("click", toggleRead);
+			let msgs = (function () {
+				readNode.addEventListener("mouseenter", function (e) {
+					e.stopPropagation();
+					alertgreen.textContent = "Change read status";
+					alert.textContent = "";
+				})
+				readNode.addEventListener("mouseleave", function (e) {
+					e.stopPropagation();
+					alertgreen.textContent = "";
+				})
+				tr.addEventListener("mouseenter", (e) => {
+					e.stopPropagation();
+					alert.textContent = "Delete entry";
+					alertgreen.textContent = "";
+				})
+				tr.addEventListener("mouseleave", (e) => {
+					e.stopPropagation();
+					alert.textContent = "";
+				})
+			})()
+			function toggleRead (e) {
+				e.stopPropagation();
+				element.read = (() => {
+					return element.read === "not read" ? "read" : "not read"
+				})()
+				readNode.textContent = element.read;
+			}
+			function remove (tr) {
+				tr.addEventListener("click", () => {
+					tbody.removeChild(tr);
+					shelf.splice(i-1, 1);
+	})}}};
+	form.addEventListener("submit", (event) => {
+		const title = document.querySelector("#title").value,
+		author = document.querySelector("#author").value,
+		pages = document.querySelector("#pages").value,
+		read = document.querySelector("#read").checked;
+		event.preventDefault();
+		let newBook = new Library (title, author, pages, read);
+		newBook.addToShelf(newBook);
+})})()
